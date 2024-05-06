49d4c2f6b57badfcbef9bae243174759c9f8fd54
Paul Alekseev <paul.alekseev@icloud.com>
2024-05-05 20:45:01 -0700

    add truncate for text

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -20,10 +20,12 @@ export const Card: FC<CardProps> = ({
       <div className="author-info">
         <img src={image} alt="Author" />
         <div className="author-names">
-          <p className="name text-lg font-semibold text-primary">
+          <p className="name text-lg font-semibold text-primary truncate">
             {authorName}
           </p>
-          <p className="nickname text-sm text-secondary">{authorNickname}</p>
+          <p className="nickname text-sm text-secondary truncate">
+            {authorNickname}
+          </p>
         </div>
       </div>
       <p className="text text-base text-secondary">{text}</p>
